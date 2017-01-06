47dfae3fc39a3c25f62bf5dedfcb659512ae667a
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-01-06 13:01:48 +0100

    Passing test

--- test/fft/core_test.clj
+++ test/fft/core_test.clj
@@ -5,18 +5,33 @@
             [fft.core :refer :all]
             [clojure.algo.generic.math-functions :as gmf]))
 
+(defn
+  cmp
+  "Compares collections of Complex numbers"
+  [a b]
+  (every?
+    true?
+    (map-indexed
+      (fn [idx itm]
+        (equals itm (nth a idx) 0.1))
+      b)))
+
 (deftest calculates_fft
-  (are [x y] (= x y) 
-       (fft [0]) [(complex 0 0)]
-       (fft [1]) [(complex 1 0)]
-
-       (fft [1 0]) [(complex 1 0) (complex  1 0)]
-       (fft [0 1]) [(complex 1 0) (complex -1 0)]))
-
-      ; (fft [0 1 2 3])
-      ; [(complex  6  0)
-      ;  (complex -2  2)
-      ;  (complex -2  0)
-      ;  (complex -2 -2)]))
-
-;(gmf/approx= 2.01 2.0 1e-2)
+  (is (true? (cmp
+               (fft [0])
+               [(complex 0 0)])))
+  (is (true? (cmp
+               (fft [1])
+               [(complex 1 0)])))
+  (is (true? (cmp
+               (fft [1 0])
+               [(complex 1 0) (complex  1 0)])))
+  (is (true? (cmp
+               (fft [0 1])
+               [(complex 1 0) (complex -1 0)])))
+  (is (true? (cmp
+               (fft [0 1 2 3])
+               [(complex  6  0)
+                (complex -2  2)
+                (complex -2  0)
+                (complex -2 -2)]))))
