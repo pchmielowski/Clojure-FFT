e564a010468542a3c4671711a391647fa37fb9bc
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-01-06 16:42:07 +0100

    Divide by two FFT. With bug

--- src/fft/core.clj
+++ src/fft/core.clj
@@ -28,4 +28,34 @@
     )
   )
 
-(defn fft [samples] (dft samples))
+(defn fft
+  [samples]
+  (def length (count samples))
+  (defn rotate
+    [idx sample]
+    (* sample
+       (exp
+         (-
+          (/
+           (* 2 Math/PI i)
+           length)
+          idx))))
+  (defn evn [input] (take-nth 2 input))
+  (defn odd [input] (take-nth 2 (rest input)))
+  (defn twotimes [input] (reduce into (repeat 2 input)))
+  (if
+    (= length 1)
+    (map (fn [s] (complex s)) samples)
+    (map
+      +
+      (twotimes
+        (dft (evn samples)))
+      (map-indexed
+        rotate
+        (twotimes
+          (dft (odd samples)))))))
+
+(println "FFT")
+(println (fft [0 1]))
+(println "DFT")
+(println (dft [0 1]))
