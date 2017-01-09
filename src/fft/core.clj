6196fbcdaaff011da1ece3c2a98f94941c9b390c
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-01-09 19:24:24 +0100

    make-rotating-function

--- src/fft/core.clj
+++ src/fft/core.clj
@@ -29,17 +29,20 @@
     )
   )
 
-(defn fft
-  [samples]
-  (def length (count samples))
-  (defn rotate
+(defn make-rotating-function
+  [N]
+  (defn rotate-sample
     [idx sample]
     (*
       sample
       (exp
         (/
           (* -2 Math/PI i idx)
-          length))))
+          N)))))
+
+(defn fft
+  [samples]
+  (def length (count samples))
   (defn evn [input] (take-nth 2 input))
   (defn odd [input] (take-nth 2 (rest input)))
   (defn twotimes [input] (reduce into (repeat 2 input)))
@@ -51,6 +54,6 @@
       (twotimes
         (dft (evn samples)))
       (map-indexed
-        rotate
+        (make-rotating-function length)
         (twotimes
           (dft (odd samples)))))))
