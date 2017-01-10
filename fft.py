3694b6e971d2f955ee7c19a78842f50b90f01544
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2017-01-10 19:55:39 +0100

    Automatic test creation

--- fft.py
+++ fft.py
@@ -1,12 +1,18 @@
 import numpy as np
 
-def f (x): print np.fft.fft(np.array(x))
+def create_test(x):
+    print
+    print '(is(cmp'
+    print('(fft [' + ' '.join('{}'.format(k) for k in x) + '])')
+    y = np.fft.fft(np.array(x))
+    print('[' + '\n'.join('( complex {r} {i})'.format(r=k.real, i=k.imag) for k in y) + ']))')
 
-f([1])
-f([0])
+create_test([1])
+create_test([0])
 
-f([1, 0])
-f([0, 1])
+create_test([1, 0])
+create_test([0, 1])
 
-f([0, 1, 2, 3])
-f([0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3])
+create_test([0, 1, 2, 3])
+create_test([0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3])
+create_test([-1, -100, 100, 1000])
